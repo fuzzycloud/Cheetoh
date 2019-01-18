3b4fc7c9904d122b1845463e1e6e452b658b499d
Kunjan Dalal <kunjee17@gmail.com>
2019-01-18 11:48:34 +0530

    Ported to razor page for better html support

--- src/Server/Server.fs
+++ src/Server/Server.fs
@@ -4,12 +4,12 @@ open Giraffe
 open Microsoft.AspNetCore.Http
 open User
 open Dummy
-open Cheetoh.Views
+open Giraffe.Razor
 
 let serverPages : HttpFunc -> HttpContext -> HttpFuncResult =
-    choose [ GET >=> route "/about" >=> (About.root() |> htmlView)
+    choose [ GET >=> route "/about" >=> (razorHtmlView "About" None None)
              GET >=> route "/admin" >=> htmlFile "index.html"
-             GET >=> route "/" >=> (Home.root() |> htmlView) ]
+             GET >=> route "/" >=> (razorHtmlView "Home" None None) ]
 
 let webApp : HttpFunc -> HttpContext -> HttpFuncResult =
     choose [ serverPages; cheetohApp; createUserApp ]
