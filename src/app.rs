d5606552b13732be28f367599a4d0e7465f3e724
Kunjan Dalal <kunjee17@gmail.com>
2024-02-28 23:13:51 +0530

    tailwind ui and daisyui added

--- src/app.rs
+++ src/app.rs
@@ -11,7 +11,7 @@ pub fn App() -> impl IntoView {
         // injects a stylesheet into the document <head>
         // id=leptos means cargo-leptos will hot-reload this stylesheet
         <Stylesheet id="leptos" href="/pkg/cheetoh.css"/>
-
+        <Link rel="shortcut icon" type_="image/ico" href="/favicon.ico"/>
         // sets the document title
         <Title text="Welcome to Leptos"/>
 
@@ -30,13 +30,25 @@ pub fn App() -> impl IntoView {
 /// Renders the home page of your application.
 #[component]
 fn HomePage() -> impl IntoView {
-    // Creates a reactive value to update the button
     let (count, set_count) = create_signal(0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
 
     view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        <main class="my-0 mx-auto max-w-3xl text-center">
+            <h2 class="p-6 text-4xl">"Welcome to Leptos with Tailwind"</h2>
+            <p class="px-10 pb-10 text-left">"Tailwind will scan your Rust files for Tailwind class names and compile them into a CSS file."</p>
+            <button
+                class="bg-amber-600 hover:bg-sky-700 px-5 py-3 text-white rounded-lg"
+                on:click=move |_| set_count.update(|count| *count += 1)
+            >
+                "Something's here | "
+                {move || if count.get() == 0 {
+                    "Click me!".to_string()
+                } else {
+                    count.get().to_string()
+                }}
+                " | Some more text"
+            </button>
+        </main>
     }
 }
 
